99a5b27b6442e75359708dd373d23dd473b235e1
Batov <i.batov@megaplan.ru>
2018-03-25 21:05:53 +0300

    first commit

---
+++ src/com/example/helloworld/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
---
+++ src/twoSum/twoSumTest.java
@@ -0,0 +1,7 @@
+package twoSum;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class twoSumTest {
+
+}
